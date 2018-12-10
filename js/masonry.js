6b986fde7425394ab8b249b5da258c151d82bc76
marcosvillabasa <marcosvillabasa@gmail.com>
2018-12-10 11:40:50 -0300

    agregado masonry para la seccion clientes

---
+++ js/masonry.js
@@ -0,0 +1,13 @@
+$('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 320
+});
+
+// $(document).ready(function(){
+//   $('.grid').masonry({
+//     // options
+//     itemSelector: '.grid-item',
+//     columnWidth: 200
+//   });
+// });
