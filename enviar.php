9c2e7304da2103f2e66fc0b40ff3059379f49f86
marcosvillabasa <marcosvillabasa@gmail.com>
2018-12-12 15:22:27 -0300

    implementado el formulario

---
+++ enviar.php
@@ -0,0 +1,39 @@
+<?php
+$enviado = false;
+$errores = "";
+
+$nommbre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+//verificamos los datos
+if (!empty($nombre)) {
+    $nombre = trim($nombre);
+    $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+}
+
+if (!empty($email)) {
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+}
+
+if (!empty($mensaje)) {
+    $mensaje = htmlspecialchars($mensaje);
+    $mensaje = trim($mensaje);
+    $mensaje = stripslashes($mensaje);
+}
+
+//datos para el correo
+$destinatario = "marcosvillabasa@gmail.com";
+$asunto = "Contacto desde la WEB";
+
+$mensajePreparado = "De: $nommbre \n";
+$mensajePreparado .= "Email: $email \n";
+$mensajePreparado .= "Mensaje: $mensaje";
+
+//enviando mensaje
+mail($destinatario,$asunto,$mensajePreparado);
+header('Location:index.html');
+$enviado=true;
+
+
+?>
--- js/validacion.js
+++ js/validacion.js
@@ -4,14 +4,17 @@ $(document).ready(function(){
   var nombre = $('#nombre');
   var email = $('#email');
   var mensaje = $('#mensaje');
+  var enviado = false;
 
   function valNombre(e){
 
     if (nombre.val() ==  '' || nombre.val() == null) {
       e.preventDefault();
       $('input[type="text"] + .error').css('display','block');
+      enviado=false;
     }else {
       $('input[type="text"] + .error').css('display','none');
+      enviado=true;
     }
   }
 
@@ -20,8 +23,10 @@ $(document).ready(function(){
     if (email.val() ==  '' || email.val() == null) {
       e.preventDefault();
       $('input[type="email"] + .error').css('display','block');
+      enviado=false;
     }else {
       $('input[type="email"] + .error').css('display','none');
+      enviado=true;
     }
   }
 
@@ -30,15 +35,30 @@ $(document).ready(function(){
     if (mensaje.val() ==  '' || mensaje.val() == null) {
       e.preventDefault();
       $('textarea + .error').css('display','block');
+      enviado=false;
     }else {
       $('textarea + .error').css('display','none');
+      enviado=true;
     }
   }
+
+  function confirmacion(e){
+    if(enviado){
+      $('.success').css('display','block');
+    }
+    else{
+      $('.success').css('display','none');
+    }
+  }
+  setInterval(confirmacion(),2000);
+
   function validacion(e){
     valNombre(e);
     valEmail(e);
     valMensaje(e);
+    confirmacion(e);
   }
+  
   formulario.on('submit',validacion);
   
 })
